51648e434a38bb71c98894a6fda1bfe6d2aad1d5
WhyNotKo <mr.areshek@vk.com>
2022-09-20 10:23:03 +0300

    type check

--- Aleshko_Labs/Aleshko_Lab1.cpp
+++ Aleshko_Labs/Aleshko_Lab1.cpp
@@ -4,7 +4,7 @@
 #include "pch.h"
 #include <iostream>
 #include <fstream>
-#include<string>
+#include <string>
 
 
 using namespace std;
@@ -14,7 +14,7 @@ struct Pipe
 
 	int length;
 	int diameter;
-	bool in_work;
+	string in_work;
 	
 };
 
@@ -31,30 +31,61 @@ void cout_menu()
 	cout << " 1. Добавить трубу\n 2. Добавить КС\n 3. Просмотр всех объектов\n 4. Сохранить\n 5. Загрузить\n 0. Выход\n";
 }
 
-//int isitint(string a)
-//{
-//
-//}
-int input_menu()
+string input_bool()
 {
 	while (true)
 	{
-		/*int a;
-		cin >> a;
-		if ((a < 0) or (a > 5) or (!cin))
-		{
-			cout << "Введите корректное число..." << endl;
-		}
-		else
-		{
-			return a;
-		}
-		cin.clear();
-		fflush(stdin);*/
 		string str;
 		cin >> str;
 		bool err = true;
-		for (int i = 0; i < 3; i++)
+		if ((str[0] != 'y') && (str[0] != 'n')) err = false;
+
+		if (!err)
+		{
+			cout << "Введите корректное значение..." << endl;
+			!err;
+			continue;
+		}
+		return str;
+	}
+}
+
+int input_int()
+{
+	while (true)
+	{
+		string str;
+		cin >> str;
+		bool err = true;
+		for (auto c : str)
+		{
+			if ((c-48 >0) or (c-48 <10))
+			{
+				!err;
+				break;
+			}
+		}
+
+		if (!err)
+		{
+			cout << "Введите корректное число..." << endl;
+			!err;
+			//cin.clear();
+			continue;
+		}
+		else return stoi(str);
+	}
+
+}
+int input_menu()
+{
+	string str;
+	while (true)
+	{
+		
+		cin >> str;
+		bool err = true;
+		for (int i = 0; i < str.length(); i++)
 		{
 			if ((str.length()!=1) or (str[0]-48>5))
 			{
@@ -62,7 +93,6 @@ int input_menu()
 				break;
 			}
 		}
-		//cout << str + " " << (int)str[0]<<" "<<str[0] + 5 << endl;
 
 		if (!err)
 		{
@@ -79,23 +109,13 @@ int input_menu()
 
 int main()
 {
-
 	setlocale(LC_ALL, "");
 
 	Pipe pipe;
-	pipe.in_work = 0;
 	int menu = 8;
-	int len, dim;
-	bool sost;
-	/*pipe.length = 0 ;
-	pipe.diameter = 0;
-	pipe.in_work = 0;*/
 
 
 	C_stat comp;
-	string name = "";
-	double eff = 0;
-	int all = 0, work = 0;
 	cout_menu();
 	while (menu)
 	{
@@ -109,9 +129,10 @@ int main()
 		if (menu == 1)
 		{
 			system("cls");
-			cout << "Введите длину, диаметр и состояние трубы(1/0) через пробел" << endl;
-			
-			cin >> pipe.length >> pipe.diameter >> pipe.in_work;
+			cout << "Введите длину, диаметр и состояние трубы(y/n) через пробел" << endl;
+			pipe.length = input_int();
+			pipe.diameter = input_int();
+			pipe.in_work = input_bool();
 			cout_menu();
 
 			//cout << pipe.length << " " << pipe.diameter << " " << pipe.in_work << endl;
@@ -120,12 +141,11 @@ int main()
 		{
 			system("cls");
 			cout << "Введите название, кол-во цехов, работающих цехов и эффективность через пробел" << endl;
-
-			cin >> comp.name >> comp.all_count >> comp.work_count >> comp.efficiency;
-			/*comp.name = name;
-			comp.all_count = all;
-			comp.work_count = work;
-			comp.efficiency = eff;*/
+			
+			cin >> comp.name;
+			comp.all_count = input_int();
+			comp.work_count = input_int(); 
+			comp.efficiency = input_int();
 
 			cout_menu();
 		}
