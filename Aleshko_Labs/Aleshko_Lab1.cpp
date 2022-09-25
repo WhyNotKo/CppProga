630061d07e02f1c743deff456fa4d64bdafab29c
WhyNotKo <mr.areshek@vk.com>
2022-09-25 12:58:06 +0300

    change for pipe and cs
    a takzhe nemnogo kostiley

--- Aleshko_Labs/Aleshko_Lab1.cpp
+++ Aleshko_Labs/Aleshko_Lab1.cpp
@@ -29,7 +29,7 @@ struct C_stat
 void cout_menu()
 {
 	system("cls");
-	cout << " 1. Добавить трубу\n 2. Добавить КС\n 3. Просмотр всех объектов\n 4. Сохранить\n 5. Загрузить\n 0. Выход\n";
+	cout << "\n 1. Добавить трубу\n 2. Добавить КС\n 3. Просмотр всех объектов\n 4. Сохранить\n 5. Загрузить\n 6. Изменить трубу\n 7. Изменить КС\n 0. Выход\n";
 }
 
 string input_bool()
@@ -47,6 +47,8 @@ string input_bool()
 			!err;
 			continue;
 		}
+		if (str[0] == 'y') str = "Работает";
+		else str = "Ремонтируется";
 		return str;
 	}
 }
@@ -89,7 +91,7 @@ int input_menu()
 		bool err = true;
 		for (int i = 0; i < str.length(); i++)
 		{
-			if ((str.length()!=1) or (str[0]-48>5))
+			if ((str.length()!=1) or (str[0]-48>7))
 			{
 				err = false;
 				break;
@@ -115,7 +117,7 @@ void cout_obj(Pipe pipe, C_stat comp)
 	cout << "Длина трубы: " << pipe.length << " км\nДиаметр трубы: " << pipe.diameter << " мм\nСостояние: " << pipe.in_work << endl;
 	cout << "\nНазвание КС: " << comp.name << " \nКоличество цехов: " << comp.all_count << " \nКоличество работающих цехов: " << comp.work_count << " \nЭффективность: " << comp.efficiency << endl;
 
-	cout << "\n 1. Добавить трубу\n 2. Добавить КС\n 3. Просмотр всех объектов\n 4. Сохранить\n 5. Загрузить\n 0. Выход\n";
+	cout << "\n 1. Добавить трубу\n 2. Добавить КС\n 3. Просмотр всех объектов\n 4. Сохранить\n 5. Загрузить\n 6. Изменить трубу\n 7. Изменить КС\n 0. Выход\n";
 }
 
 void input_pipe(Pipe& pipe)
@@ -188,28 +190,60 @@ void load_data(Pipe& pipe, C_stat& comp)
 	}
 }
 
+void change_pipe(Pipe& pipe)
+{
+	cout << "Введите состояние трубы(y/n) (у если работает, n если в ремонте)" << endl;
+	pipe.in_work = input_bool();
+	cout_menu();
+}
+
+void change_cs(C_stat& comp)
+{
+	cout << "Введите количество работающих цехов КС, сейчас " << comp.work_count << " из " << comp.all_count << endl;
+	while (true)
+	{
+		int check = input_int();
+		if (check > comp.all_count)
+		{
+			cout << "Введите меньше цехов, чем их всего..." << endl;
+			continue;
+		}
+		else
+		{
+			comp.work_count = check;
+			break;
+		}
+	}
+	cout_menu();
+}
+
 int main()
 {
 	setlocale(LC_ALL, "");
 
 	Pipe pipe;
+	pipe.length = 0;
 	C_stat comp;
-	int menu = 8;
+	comp.all_count = 0;
+	int menu ;
 	cout_menu();
-	while (menu)
+	while (true)
 	{
 		
 		menu = input_menu();
 		if (menu == 0)
 		{
-			break;
 			cout_menu();
+			continue;
+			
 		}
-		if (menu == 1) input_pipe(pipe);
-		if (menu == 2) input_cp(comp);
+		if (menu == 1) if (!pipe.length) input_pipe(pipe); else cout << "Труба уже существует" << endl;
+		if (menu == 2) if (!comp.all_count) input_cp(comp); else cout << "КС уже существует" << endl;
 		if (menu == 3) cout_obj(pipe, comp);
 		if (menu == 4) save_data(pipe, comp);
 		if (menu == 5) load_data(pipe, comp);
+		if (menu == 6) if (pipe.length) change_pipe(pipe); else cout << "Труба не существует" << endl;
+		if (menu == 7) if (comp.all_count) change_cs(comp); else cout << "КС не существует" << endl;
 	}
 	return 0;
 }
